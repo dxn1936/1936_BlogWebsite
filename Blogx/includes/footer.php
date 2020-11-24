0e380069e5304a40aa18105014e37851948d12ad
dxn1936 <mca.1936@unigoa.ac.in>
2020-11-24 21:59:10 +0530

    Blog Website implementation

---
+++ Blogx/includes/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+			<p>MyViewers &copy; <?php echo date('Y'); ?></p>       <!--for entering year-->
+		</div>
+		<!-- // footer -->
+
+	</div>
+	<!-- // container -->
+</body>
+</html>
---
+++ Blogx/includes/head-section.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Google Fonts -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
+	<!-- Styling for public area -->
+	<link rel="stylesheet" href="static/css/style.css">
+	<meta charset="UTF-8">
